2ddebae559183763cdce00b68eb693a0b806b119
Maduranga Lakjeewa <lakjeewa@users.noreply.github.com>
2016-09-20 13:50:16 +0530

    Map full view.

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -1,6 +1,5 @@
 angular.module('MainCtrl', ['ngMap']).controller('MainController', function ($scope, NgMap) {
     $scope.googleMapsUrl = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBdU7aA7mrqoL9HHfPqlUaX-zsjlPNqhas";
-    $scope.tagline = 'Show the map here!';
     
     NgMap.getMap().then(function (map) {
         console.log(map.getCenter());
