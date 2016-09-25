543be023f9334e3b9dd8c2056d16c75fee2dc144
chamath <chamath.kumarasinghe@gmail.com>
2016-09-25 09:37:12 +0530

    Displaying company locations on the map

--- public/js/controllers/LandscapeController.js
+++ public/js/controllers/LandscapeController.js
@@ -1,5 +1,22 @@
-function LandscapeController($scope, NgMap) {
-    $scope.googleMapsUrl = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBdU7aA7mrqoL9HHfPqlUaX-zsjlPNqhas";
+function LandscapeController($scope, NgMap, companyService) {
+    $scope.googleMapsUrl = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBdU7aA7mrqoL9HHfPqlUaX-zsjlPNqhas"
+
+    $scope.markers = [];
+
+    companyService.browse().then(function (res) {
+        var companyData = res.data;
+        console.log(companyData);
+        for (var i = 0; i < companyData.length; i++) {
+            var companyLocation = {};
+            companyLocation.title = companyData[i].companyName;
+            companyLocation.latitude = companyData[i].latitude;
+            companyLocation.longitude = companyData[i].longitude;
+            if (companyLocation.latitude !== "" && companyLocation.longitude !== "") {
+                $scope.markers.push(companyLocation);
+            }
+        }
+        console.log($scope.markers);
+    });
 
     NgMap.getMap().then(function (map) {
         console.log(map.getCenter());
