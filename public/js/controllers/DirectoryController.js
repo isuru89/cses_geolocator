f9f3c00e124d5a2d2fb71b0be0f777cc48196801
chamath <chamath.kumarasinghe@gmail.com>
2016-09-25 12:57:22 +0530

    Changing location of the company details view

--- public/js/controllers/DirectoryController.js
+++ public/js/controllers/DirectoryController.js
@@ -2,6 +2,11 @@ function DirectoryController($scope, companyService) {
 
     companyService.browse().then(function (res) {
         $scope.companies = res.data;
+        $scope.selectedCompany = $scope.companies[0];
     });
+
+    $scope.setSelectedCompany = function (param){
+        $scope.selectedCompany = param;
+    };
 
 }
