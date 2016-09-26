33abf2a4f9296443598097a6eab1c1888680c622
Maduranga Lakjeewa <lakjeewa@users.noreply.github.com>
2016-09-26 12:06:06 +0530

    app config to a new line.

--- public/js/app.js
+++ public/js/app.js
@@ -2,27 +2,27 @@ var app = angular.module('geoLocatorApp', ['ui.router', 'CompanyService', 'ngMap
 
 app.controller('LandscapeController', LandscapeController)
         .controller('DirectoryController', DirectoryController)
-        .controller('CompanyDetailController', CompanyDetailController)
-        .config(function ($stateProvider) {
-                var landscapeState = {
-                        name: 'landscape',
-                        url: '/landscape/:companyId',
-                        templateUrl: 'views/landscape.html',
-                        activetab: 'landscape'
-                }
+        .controller('CompanyDetailController', CompanyDetailController);
 
-                var directoryState = {
-                        name: 'directory',
-                        url: '/directory',
-                        templateUrl: 'views/directory.html',
-                        activetab: 'directory'
-                }
+app.config(function ($stateProvider, $urlRouterProvider) {
+    var landscapeState = {
+        name: 'landscape',
+        url: '/landscape/:companyId',
+        templateUrl: 'views/landscape.html',
+        activetab: 'landscape'
+    };
 
-                $stateProvider.state(landscapeState);
-                $stateProvider.state(directoryState);
-        }).
-        config(function ($urlRouterProvider) {
-                $urlRouterProvider.when('', '/landscape/');
-        });
+    var directoryState = {
+        name: 'directory',
+        url: '/directory',
+        templateUrl: 'views/directory.html',
+        activetab: 'directory'
+    };
+
+    $stateProvider.state(landscapeState);
+    $stateProvider.state(directoryState);
+
+    $urlRouterProvider.when('', '/landscape/');
+});
 
 
